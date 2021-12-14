73e4fe39fa75b577601a9b552d6c596d1660db20
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-12-14 13:56:00 -0600

    uuid implementado
    modelo de tarea y tareas implementado

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('colors');
 
 //const {mostrarMenu, pausa} = require('./helpers/mensajes');
 const {inquirerMenu, pausa} = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 console.clear();
 
@@ -19,11 +21,17 @@ console.clear();
 }; */
 
 const main = async() => {
-    console.log('Hola mundo!');
-
     let opt = '';
+
     do {
-        opt = await inquirerMenu();
+        //opt = await inquirerMenu();
+        const tareas = new Tareas()
+        const tarea = new Tarea('Comprar leshe xd');
+
+        tareas._listado[tarea.id] = tarea;
+
+        console.log(tareas)
+
         await pausa();
     } while (opt !== '0');
 
