841572f272d08807064e0407ad6010d53ca89f00
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-12-14 22:54:02 -0600

    tareas completadas y pendientes implementadas

--- models/tareas.js
+++ models/tareas.js
@@ -30,12 +30,34 @@ class Tareas {
     }
 
     listadoCompleto() {
-        console.log('')
+        console.log('');
         this.listadoArr.map(({desc, completadoEn}, i) => {
             console.log(
                 `${colors.green(i + 1)}. ${desc} :: ${(completadoEn) ? 'Completado'.green : 'Pendiente'.red}`
             );
         })
+    }
+
+    listarPendientesCompletadas( completadas = true ) {
+        console.log('');
+        
+        if (completadas) {
+            let aux = 1;
+            this.listadoArr.map(({desc, completadoEn}) => {
+                if(completadoEn != null) {
+                    console.log(`${colors.green(aux)}. ${desc} :: ${'Completado'.green}`);
+                    aux++;
+                }
+            });
+        } else {
+            let aux = 1;
+            this.listadoArr.map(({desc, completadoEn}) => {
+                if(completadoEn === null) {
+                    console.log(`${colors.green(aux)}. ${desc} :: ${'Pendiente'.red}`);
+                    aux++;
+                }
+            });
+        }
     }
 }
 
